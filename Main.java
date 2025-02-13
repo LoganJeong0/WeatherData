29dfdb61cd1b3fcb8e2cedb3b1196fc111a3676a
LoganJeong0 <loganjeong0@gmail.com>
2025-02-13 17:13:54 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args){
+        double[] temps = {99.1, 142, 85, 86.1, 84.6, 94.3, 123, 98, 101};
+        ArrayList<Double> t = new ArrayList<Double>();
+        for(double n : temps) t.add(n);
+        WeatherData w = new WeatherData(t);
+        //w.cleanData(85.0, 120.0);
+        System.out.println(w.longestHeatWave(95.2));
+    }
+}
---
+++ WeatherData.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+public class WeatherData
+{
+/** Guaranteed not to be null and to contain only non-null entries */
+private ArrayList<Double> temperatures;
+public WeatherData(ArrayList<Double> arr){
+    temperatures = arr;
+}
+public void cleanData(double lower, double upper)
+{ 
+    for (int i = 0; i < temperatures.size(); i++){
+        if (temperatures.get(i) > upper || temperatures.get(i) < lower){
+            temperatures.remove(i);
+        }
+    }
+}
+public ArrayList<Double> getArrayList(){
+    return temperatures;
+}
+public int longestHeatWave(double threshold)
+{ 
+    int max = 0;
+    int current = 0;
+    for (double heat : temperatures){
+        System.out.println("heat: " + heat + "   Threshold: " + threshold + "  Result: " + (heat > threshold));
+        if (heat > threshold){
+            //System.out.println("heat: " + heat + "   Threshold: " + threshold + "  Result: " + (heat > threshold));
+            current++;
+        }
+        else current = 0;
+        if (current > max){
+            max = current;
+        }
+    }   
+    return max;
+}
+// There may be instance variables, constructors, and methods that are not shown.
+}
